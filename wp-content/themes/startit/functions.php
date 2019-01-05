5b668276549a9fb1b161e622ffdaead98085f256
Andronov <sab2rih@gmail.com>
2019-01-05 12:26:03 +0200

    Update functions.php
    connecting scripts through wp_register

--- wp-content/themes/startit/functions.php
+++ wp-content/themes/startit/functions.php
@@ -2,17 +2,32 @@
 add_action('wp_enqueue_scripts', 'my_scripts_method'); //по событию вызов функции подключения
 
 function my_scripts_method() {
-	wp_enqueue_style('stylesheet', get_template_directory_uri() . "/style.css");
 
-	wp_deregister_script('jquery'); //перерегистрация вывод jquery в футере
-	wp_register_script('jquery', includes_url('js/jquery/jquery.js', false, null, true));
+	wp_register_style('stylesheet', get_template_directory_uri() . "/style.css", array(), null, 'all');
+	wp_enqueue_style('stylesheet');
+
+	wp_register_script('jquery', get_template_directory_uri() . 'js/jquery/jquery.js', false, null, true);
 	wp_enqueue_script('jquery');
 
-	wp_enqueue_script('jquery', get_template_directory_uri() . "/js/jquery-3.2.1.min.js", false, null, true/*true-выводится в футере*/);
-	wp_enqueue_script('jquery_mixitup', get_template_directory_uri() . "/js/jquery.mixitup.min.js", false, null, true);
-	wp_enqueue_script('jquery_fancybox', get_template_directory_uri() . "/js/jquery.fancybox.min.js", false, null, true);
-	wp_enqueue_script('owl', get_template_directory_uri() . "/js/owl.carousel.js", false, null, true);
-	wp_enqueue_script('typed', get_template_directory_uri() . "/js/typed.min.js", false, null, true);
-	wp_enqueue_script('menu', get_template_directory_uri() . "/js/menu.js", false, null, true);
-	wp_enqueue_script('custom', get_template_directory_uri() . "/js/custom.js", false, null, true);
+	wp_register_script('jquery-3', get_template_directory_uri() . "/js/jquery-3.2.1.min.js", false, null, true);
+	wp_enqueue_script('jquery-3');
+
+	wp_register_script('jquery_mixitup', get_template_directory_uri() . "/js/jquery.mixitup.min.js", false, null, true);
+	wp_enqueue_script('jquery_mixitup');
+
+	wp_register_script('jquery_fancybox', get_template_directory_uri() . "/js/jquery.fancybox.min.js", false, null, true);
+	wp_enqueue_script('jquery_fancybox');
+
+	wp_register_script('owl', get_template_directory_uri() . "/js/owl.carousel.js", false, null, true);
+	wp_enqueue_script('owl');
+
+	wp_register_script('typed', get_template_directory_uri() . "/js/typed.min.js", false, null, true);
+	wp_enqueue_script('typed');
+
+	wp_register_script('menu', get_template_directory_uri() . "/js/menu.js", false, null, true);
+	wp_enqueue_script('menu');
+
+	wp_register_script('custom', get_template_directory_uri() . "/js/custom.js", false, null, true);
+	wp_enqueue_script('custom');
+
 }
